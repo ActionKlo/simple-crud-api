644572ff97d1b99b562bd3f828dd03a3824f49cf
ActionKlo <actionclaw@gmail.com>
2021-11-24 14:05:23 +0100

    refactor: little refactor post.js

--- src/routes/post.js
+++ src/routes/post.js
@@ -3,19 +3,7 @@ const person = require('../services/person')
 
 exports.post = (req, res) => {
 	const url = req.url
-	console.log(randomUUID())
-
-	// if (url.split('/').length == 2) 
-
-	// let uuid = url.split('/')[2]
-	// console.log(uuid)
-	// console.log(uuid.match(/\/person\/([0-9a-f]+\-?)+/))
-	// if (url.match(/\/person\/([0-9a-f]+\-?)+/)) {
-	// 	console.log(url.match(/\/person\/([0-9a-f]+\-?)+/))
-	// 	console.log("if " + url)
-	// 	res.end()
-	// }
-
+	
 	switch (url) {
 		case '/person':
 			person.create(req, res)
